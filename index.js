6424ac28615fabf61493912fedd687df7c5d0c15
tridean <tridean97@gmail.com>
2023-07-30 16:45:35 -0500

    npm install and necessary packages

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [];
